bf66395ffc6ccc809af01141f9c1c02ac7a0675b
SL <412499326@qq.com>
2017-03-27 20:33:16 +0800

    Initial Commit

---
+++ AES_iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AES_iOS
+//
+//  Created by Levi on 2017/3/27.
+//  Copyright © 2017年 SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AES_iOS/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AES_iOS
+//
+//  Created by Levi on 2017/3/27.
+//  Copyright © 2017年 SL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
